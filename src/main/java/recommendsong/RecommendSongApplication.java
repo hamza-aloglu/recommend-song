69b8c977e0ff3eae2fde325770174236cfec5fb4
hamza-aloglu <74200100+hamza-aloglu@users.noreply.github.com>
2022-09-15 12:12:16 +0300

    initialize spring web

---
+++ src/main/java/recommendsong/RecommendSongApplication.java
@@ -0,0 +1,13 @@
+package recommendsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendSongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendSongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/recommendsong/RecommendSongApplicationTests.java
@@ -0,0 +1,13 @@
+package recommendsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
