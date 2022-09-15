ebbb0cfcd50e2ae7244cbda63c9fe0f04ed1b5c3
hamza-aloglu <74200100+hamza-aloglu@users.noreply.github.com>
2022-09-15 15:19:31 +0300

    Find most repeated artist from user given tracks.
    Changed "ArrayList" to "hashmap" so that time complexity of finding most repeated artist reduced from O(n^2) to O(n).

--- src/main/java/recommendsong/HomeController.java
+++ src/main/java/recommendsong/HomeController.java
@@ -11,34 +11,52 @@ import se.michaelthelin.spotify.model_objects.specification.Track;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HomeController {
 
     @PostMapping("/tracks")
     String recommendSongs(@RequestBody String trackNames) throws IOException, ParseException, SpotifyWebApiException {
-        // Find artists of 5 songs
+        // Find artists of 5 songs.Artists represented by id.
         String[] trackNamesArray = trackNames.split(",");
-        String spotifyAccessToken = "";
+        String spotifyAccessToken = "BQAOmB6w4NEU3joyCtw0O0D4B7lJGPX82E-nR2Da8RdCTPuc09wk5LvaCj0qK093Zy1GjiVl2vkij33DdyevGCAKWf3-Kw24PJAhYqTu-tBCBgGb1sW5pQV5ArHO3w0u7tx8gQiv2S0KWrm5CewdgKWVl738olkV50SqDZNfhr-SRphEKuNhqhCFG38JgHY";
 
         final SpotifyApi spotifyApi = new SpotifyApi.Builder()
                 .setAccessToken(spotifyAccessToken)
                 .build();
 
-        List<Artist> artists = new ArrayList<>();
+        Map<String, Integer> artistsWithFrequency = new HashMap<>();
 
-        for (String trackName: trackNamesArray) {
-            // Find artist id
+        for (String trackName : trackNamesArray) {
             Track track = spotifyApi.searchTracks(trackName).limit(1).build().execute().getItems()[0];
-            String  artistId = track.getArtists()[0].getId();
+            String artistId = track.getArtists()[0].getId();
 
-            // Find and add artist
-            Artist artist = spotifyApi.getArtist(artistId).build().execute();
-            artists.add(artist);
+            if (artistsWithFrequency.containsKey(artistId)) {
+                int freq = artistsWithFrequency.get(artistId);
+                freq++;
+                artistsWithFrequency.put(artistId, freq);
+            } else {
+                artistsWithFrequency.put(artistId, 1);
+            }
+
         }
 
-        // Find most duplicated artist
+        // Find most repeated artist.
+        String mostRepeatedArtistId = null;
+        int maxRepeatedNumber = 0;
+        for (String key : artistsWithFrequency.keySet()) {
+            int artistOccurrence = artistsWithFrequency.get(key);
+
+            if (artistOccurrence > maxRepeatedNumber) {
+                mostRepeatedArtistId = key;
+                maxRepeatedNumber = artistOccurrence;
+            }
+        }
+
+        Artist artist = spotifyApi.getArtist(mostRepeatedArtistId).build().execute();
 
 
         // Send random 3 songs of that artist which any of them is not any of given songs.
