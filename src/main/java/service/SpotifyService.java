cca803d639a9e1c45b48c60b1007474f5177b124
hamza-aloglu <74200100+hamza-aloglu@users.noreply.github.com>
2022-09-16 21:09:45 +0300

    make SpotifyService abstract

--- src/main/java/service/SpotifyService.java
+++ src/main/java/service/SpotifyService.java
@@ -2,7 +2,7 @@ package service;
 
 import se.michaelthelin.spotify.SpotifyApi;
 
-public class SpotifyService {
+public abstract class SpotifyService {
     private final String spotifyAccessToken = "BQAXjRh5qb9CQtLcuBeNUpGo441a8UE7gPgdvCzu94gjGr4AiX0cmAJE1AQWe9zLAGVS3O4GMA24bI91W11JyBFX_8RRASpKilFpQ4hf8M5WsMD1Afrt5ysBU4IPX8bJZux6qWThi4aZFRWN42JbsPmsaNA0Xo4gokpb9On1kUZUvVbINmo3UH3oxXSaZHc";
 
     protected final SpotifyApi spotifyApi = new SpotifyApi.Builder()
