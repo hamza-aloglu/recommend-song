c669bebbc8fd2547c30c059548d7b4c5e4f46d75
hamza-aloglu <74200100+hamza-aloglu@users.noreply.github.com>
2022-09-16 21:01:06 +0300

    Finding track ids from user given track names

---
+++ src/main/java/service/TrackService.java
@@ -0,0 +1,34 @@
+package service;
+
+import org.apache.hc.core5.http.ParseException;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrackService extends SpotifyService {
+    public List<String> findTrackIdFromName(String[] trackNames) throws IOException, ParseException, SpotifyWebApiException {
+        List<String> trackIds = new ArrayList<>();
+
+        for (String trackName: trackNames) {
+            String trackId = this.findTrackIdFromName(trackName);
+
+            if (trackIds.contains(trackId)) {
+                continue;
+            }
+            trackIds.add(trackId);
+        }
+        return trackIds;
+    }
+
+    public String findTrackIdFromName(String trackName) throws IOException, ParseException, SpotifyWebApiException {
+        return spotifyApi.searchTracks(trackName)
+                .limit(1)
+                .build()
+                .execute()
+                .getItems()[0]
+                .getId();
+    }
+}
