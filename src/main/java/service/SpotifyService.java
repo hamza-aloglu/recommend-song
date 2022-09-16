340cfb8304964e1e17f5c5f4d709debdec550cd1
hamza-aloglu <74200100+hamza-aloglu@users.noreply.github.com>
2022-09-16 21:45:23 +0300

    Define country for all spotify services

--- src/main/java/service/SpotifyService.java
+++ src/main/java/service/SpotifyService.java
@@ -1,10 +1,12 @@
 package service;
 
+import com.neovisionaries.i18n.CountryCode;
 import se.michaelthelin.spotify.SpotifyApi;
 
 public abstract class SpotifyService {
     private final String spotifyAccessToken = "BQAXjRh5qb9CQtLcuBeNUpGo441a8UE7gPgdvCzu94gjGr4AiX0cmAJE1AQWe9zLAGVS3O4GMA24bI91W11JyBFX_8RRASpKilFpQ4hf8M5WsMD1Afrt5ysBU4IPX8bJZux6qWThi4aZFRWN42JbsPmsaNA0Xo4gokpb9On1kUZUvVbINmo3UH3oxXSaZHc";
 
+    protected CountryCode country = CountryCode.TR;
     protected final SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setAccessToken(this.spotifyAccessToken)
             .build();
