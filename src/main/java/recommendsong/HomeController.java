858a8013e83a60eb3146e53be757559f467d62ae
hamza-aloglu <74200100+hamza-aloglu@users.noreply.github.com>
2022-09-16 21:44:26 +0300

    Separate code into services.

--- src/main/java/recommendsong/HomeController.java
+++ src/main/java/recommendsong/HomeController.java
@@ -9,6 +9,8 @@ import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.Track;
+import service.ArtistService;
+import service.TrackService;
 
 import java.io.IOException;
 import java.util.*;
@@ -18,84 +20,35 @@ public class HomeController {
 
     @PostMapping("/tracks")
     List<String> recommendTracks(@RequestBody String trackNames) throws IOException, ParseException, SpotifyWebApiException {
-        // Find artists of 5 songs.Artists represented by id.
         String[] trackNamesArray = trackNames.split(",");
-        String spotifyAccessToken = "BQAXjRh5qb9CQtLcuBeNUpGo441a8UE7gPgdvCzu94gjGr4AiX0cmAJE1AQWe9zLAGVS3O4GMA24bI91W11JyBFX_8RRASpKilFpQ4hf8M5WsMD1Afrt5ysBU4IPX8bJZux6qWThi4aZFRWN42JbsPmsaNA0Xo4gokpb9On1kUZUvVbINmo3UH3oxXSaZHc";
 
-        System.out.println("user given track names");
-        for (String track: trackNamesArray) {
-            System.out.println(track);
+        TrackService trackService = new TrackService();
+        List<String> userGivenTrackIds = trackService.findTrackIdFromName(trackNamesArray);
 
-        }
+        ArtistService artistService = new ArtistService();
+        Map<String, Integer> artistsWithFrequency = artistService.getArtistsWithFrequency(userGivenTrackIds);
 
-        final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-                .setAccessToken(spotifyAccessToken)
-                .build();
-
-        Map<String, Integer> artistsWithFrequency = new HashMap<>();
-        List<String> userGivenTrackIds = new ArrayList<>();
-
-        for (String trackName : trackNamesArray) {
-            Track track = spotifyApi.searchTracks(trackName).limit(1).build().execute().getItems()[0];
-
-            // Every user given track should be unique...
-            // Models: track, artist
-            if (userGivenTrackIds.contains(track.getId())) {
-                continue;
-            }
-
-            System.out.println("user given track: " + track.getName());
-
-            String artistId = track.getArtists()[0].getId();
-            userGivenTrackIds.add(track.getId());
-
-            if (artistsWithFrequency.containsKey(artistId)) {
-                int freq = artistsWithFrequency.get(artistId);
-                freq++;
-                artistsWithFrequency.put(artistId, freq);
-            } else {
-                artistsWithFrequency.put(artistId, 1);
-            }
-
-        }
-
-        // Find most repeated artist.
-        String mostRepeatedArtistId = null;
-        int maxRepeatedNumber = 0;
-        for (String key : artistsWithFrequency.keySet()) {
-            int artistOccurrence = artistsWithFrequency.get(key);
-
-            if (artistOccurrence > maxRepeatedNumber) {
-                mostRepeatedArtistId = key;
-                maxRepeatedNumber = artistOccurrence;
-            }
-        }
-
-        Artist mostRepeatedArtist = spotifyApi.getArtist(mostRepeatedArtistId).build().execute();
-        System.out.println("most repeated artist name: " + mostRepeatedArtist.getName());
+        Artist mostRepeatedArtist = artistService.findMostRepeatedArtist(artistsWithFrequency);
+        Track[] mostRepeatedArtistsTracks = artistService.findTopTracks(mostRepeatedArtist);
 
 
-        // Send random 3 songs of that artist which any of them is not any of given songs.
-        Track[] mostRepeatedArtistsTracks = spotifyApi.getArtistsTopTracks(mostRepeatedArtist.getId(), CountryCode.TR).build().execute();
-
-
-        List<String> recommendedTracks = new ArrayList<>();
-
+        // Recommendation logic
         // Starting from 100%, chance of recommending next top track will be decreased by half for each top track.
+        List<String> recommendedTracks = new ArrayList<>();
         int percentChanceForTrackAddition = 100;
-        for (Track mostRepeatedArtistsTrack: mostRepeatedArtistsTracks) {
+
+        for (Track mostRepeatedArtistsTrack : mostRepeatedArtistsTracks) {
             if (recommendedTracks.size() >= 3) {
                 break;
             }
-
             int randomNumber = this.generateRandomNumber(0, 100);
 
+            // tracks that will be given to user should not be tracks which user gave us.
             if (!userGivenTrackIds.contains(mostRepeatedArtistsTrack.getId()) && randomNumber < percentChanceForTrackAddition) {
                 recommendedTracks.add(mostRepeatedArtistsTrack.getName());
                 percentChanceForTrackAddition /= 2;
             }
         }
-
         return recommendedTracks;
     }
 
